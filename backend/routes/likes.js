977bee2dd73947b48fd5b6219d12381d9bcf2845
myusernamesthisreal <myusernamesthis@gmail.com>
2022-10-19 20:08:08 -0500

    refactor: Likes routes

--- backend/routes/likes.js
+++ backend/routes/likes.js
@@ -49,80 +49,30 @@ module.exports = function routes(app, logger) {
       try {
         const user = await jwt.verifyToken(req);
         const { id } = req.params;
-        pool.query(
-          "SELECT author FROM db.posts WHERE id = ?",
-          [id],
-          (err, result) => {
-            if (err) {
-              logger.error("Error in POST /posts/:id/like: ", err);
-              res.status(400).send({
-                message: "Error getting post",
-                success: false,
-              })
-            }
-            else if (result.length === 0) {
-              res.status(404).send({
-                message: "Post not found",
-                success: false,
-              });
-            }
-            else {
-              if (result[0].author === user.id) {
-                res.status(401).send({
-                  message: "You cannot like your own post",
-                  success: false,
-                })
-              } else {
-                pool.query("SELECT id FROM db.likes WHERE user = ? AND post = ?", [user.id, id], (err, result) => {
-                  if (err) {
-                    logger.error("Error in POST /posts/:id/like: ", err);
-                    res.status(400).send({
-                      message: "Error getting post",
-                      success: false,
-                    })
-                  }
-                  else {
-                    if (result.length > 0) {
-                      res.status(400).send({
-                        message: "You have already liked this post",
-                        success: false,
-                      })
-                    } else {
-                      pool.query(
-                        "INSERT INTO db.likes (post, user) VALUES (?, ?)",
-                        [id, user.id],
-                        (err, result) => {
-                          if (err) {
-                            logger.error("Error in POST /posts/:id/like: ", err);
-                            res.status(400).send({
-                              message: "Error liking post",
-                              success: false,
-                            })
-                          }
-                          else {
-                            res.status(201).send({
-                              message: "Post liked successfully",
-                              success: true,
-                            })
-                          }
-                        }
-                      )
-                    }
-                  }
-                }
-                )
-              }
-            }
-          }
-        )
+        const queryResult = await query("SELECT author FROM db.posts WHERE id = ?", [id]);
+        if (queryResult.length === 0)
+          throw new Error("Post not found");
+        if (queryResult[0].author === user.id)
+          throw new Error("Cannot like your own post");
+        await query("INSERT INTO db.likes (user, post) VALUES (?, ?)", [user.id, id]);
+        res.status(200).send({
+          message: "Post liked",
+          success: true,
+        })
       } catch (e) {
         logger.error("Error in POST /posts/:id/like: ", e);
-        if (e === "Invalid token") {
+        if (e.message === "Invalid token" || e.message === "Cannot like your own post") {
           res.status(401).send({
             message: "Unauthorized",
             success: false,
           })
-        } else
+        } else if (e.message === "Post not found") {
+          res.status(404).send({
+            message: "Post not found",
+            success: false,
+          })
+        }
+        else
           res.status(500).send({
             message: "Something went wrong",
             reason: e,
@@ -141,61 +91,28 @@ module.exports = function routes(app, logger) {
       try {
         const user = await jwt.verifyToken(req);
         const { id } = req.params;
-        pool.query(
-          "SELECT author FROM db.posts WHERE id = ?",
-          [id],
-          (err, result) => {
-            if (err) {
-              logger.error("Error in DELETE /posts/:id/like: ", err);
-              res.status(400).send({
-                message: "Error getting post",
-                success: false,
-              })
-            }
-            else if (result.length === 0) {
-              res.status(404).send({
-                message: "Post not found",
-                success: false,
-              });
-            }
-            else {
-              if (result[0].author === user.id) {
-                res.status(401).send({
-                  message: "You cannot unlike your own post",
-                  success: false,
-                })
-              } else {
-                pool.query(
-                  "DELETE FROM db.likes WHERE post = ? AND user = ?",
-                  [id, user.id],
-                  (err, result) => {
-                    if (err) {
-                      logger.error("Error in DELETE /posts/:id/like: ", err);
-                      res.status(400).send({
-                        message: "Error unliking post",
-                        success: false,
-                      })
-                    }
-                    else {
-                      res.status(204).send();
-                    }
-                  }
-                )
-              }
-            }
-          }
-        )
+        const queryResult = await query("SELECT author FROM db.posts WHERE id = ?", [id]);
+        if (queryResult.length === 0)
+          throw new Error("Post not found");
+        await query("DELETE FROM db.likes WHERE user = ? AND post = ?", [user.id, id]);
+        res.status(204).send();
       } catch (e) {
         logger.error("Error in DELETE /posts/:id/like: ", e);
-        if (e === "Invalid token") {
+        if (e.message === "Invalid token") {
           res.status(401).send({
             message: "Unauthorized",
             success: false,
           })
-        } else
+        } else if (e.message === "Post not found") {
+          res.status(404).send({
+            message: "Post not found",
+            success: false,
+          })
+        }
+        else
           res.status(500).send({
             message: "Something went wrong",
-            reason: e,
+            reason: e.message,
             success: false,
           })
       }
