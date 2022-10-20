15c4fd9970a6d54990def53c15ca2dfcceffb2c0
NicoleJadeVivier <nvivier@mail.smu.edu>
2022-10-19 20:12:01 -0500

    imported mui and start making navbar

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
-
+import Breadcrumbs from '@mui/material/Breadcrumbs'
+import Link from '@mui/material/Link'
+import Button from '@mui/material/Button'
 function NavBar() {
     return (
       <>
-        <h1>About</h1>
-        <p>Hello there.<br />How do you do?</p>
+      <Breadcrumbs aria-label="breadcrumb">
+        <Link underline="hover" color="inherit" href="/">
+          Feed
+        </Link>
+        <Link
+          underline="hover"
+          color="inherit"
+          href="/material-ui/getting-started/installation/"
+        >
+          Add Post 
+        </Link>
+        <Link underline="hover" color="inherit" href="/">
+          Weather
+        </Link>
+        <Button variant="contained">Hello World</Button>
+      </Breadcrumbs>
       </>
     );
   }
