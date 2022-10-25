95d41b5e4085723e073f365a610297a44ba16f11
NicoleJadeVivier <nvivier@mail.smu.edu>
2022-10-25 13:19:20 -0500

    adding a signup route

---
+++ frontend/src/api.js
@@ -0,0 +1,24 @@
+export class Api {
+    // ENTER YOUR EC2 PUBLIC IP/URL HERE
+    ec2_url = '';
+    // CHANGE THIS TO TRUE IF HOSTING ON EC2, MAKE SURE TO ADD IP/URL ABOVE
+    ec2 = false;
+    // USE localhost OR ec2_url ACCORDING TO ENVIRONMENT
+    url = this.ec2 ? this.ec2_url : 'http://localhost:8000';
+
+    async signup(username, password) {
+        try{
+            const res = await fetch(`${this.url}/users`,
+            {
+                method: "POST",
+                body: { username, password },
+            })
+            return await res.json();
+        } catch (e) {
+            console.error(e);
+            return {success: false}
+        }
+        
+
+    }
+}
--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,64 +1,40 @@
 import React from 'react'
-import Breadcrumbs from '@mui/material/Breadcrumbs'
-import Link from '@mui/material/Link'
+import { NavLink } from 'react-router-dom';
 import Button from '@mui/material/Button'
 import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
 import Toolbar from '@mui/material/Toolbar'
 import IconButton from '@mui/material/IconButton'
 import AppBar from '@mui/material/AppBar'
+import MenuIcon from '@mui/icons-material/Menu'
 
 
 function NavBar() {
-    return (
-      <>
+  return (
+    <>
 
-<Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-  
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
-    </Box>
-      <Box
-      m={1}
- //margin
-      display="flex"
-      justifyContent="space-between"
-      //alignItems="flex-start"
-    >
-      <Breadcrumbs aria-label="breadcrumb" color="primary">
-        <Link underline="hover" color="inherit" href="/">
-          Feed
-        </Link>
-        <Link
-          underline="hover"
-          color="inherit"
-          href="/material-ui/getting-started/installation/"
-        >
-          Add Post 
-        </Link>
-        <Link underline="hover" color="inherit" href="/">
-          Weather
-        </Link>
-      </Breadcrumbs>
-      <Button variant="contained" color="primary">Sign In</Button>
-    </Box>
-      </>
-    );
-  }
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              News
+            </Typography>
+            <Button color="inherit" onClick={() => window.location.href="/signin"} >Sign In</Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </>
+  );
+}
 
 export default NavBar
 
