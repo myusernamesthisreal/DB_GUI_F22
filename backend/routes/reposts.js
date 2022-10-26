6e8bbcc049cd33ad4554ecdb61e590a29b592b3b
myusernamesthisreal <myusernamesthis@gmail.com>
2022-10-26 17:38:53 -0500

    refactor: Use promisify for neater code

--- backend/routes/reposts.js
+++ backend/routes/reposts.js
@@ -1,8 +1,9 @@
 const pool = require('../db');
 const jwt = require("../jwt");
+const promisify = require("util").promisify;
 
 module.exports = function routes(app, logger) {
-
+    const query = promisify(pool.query).bind(pool);
     // GET /posts/:id/reposts (get reposts on a post)
     app.get("/posts/:id/reposts",
         /**
@@ -12,45 +13,28 @@ module.exports = function routes(app, logger) {
         async (req, res) => {
             try {
                 const { id } = req.params;
-                pool.query("SELECT id FROM db.posts WHERE id = ?", [id], (err, result) => {
-                    if (err) {
-                        logger.error("Error in GET /posts/:id/reposts: ", err);
-                        res.status(500).send({
-                            message: "Error getting reposts",
-                            success: false,
-                        })
-                    }
-                    else if (result.length === 0) {
-                        res.status(404).send({
-                            message: "Post not found",
-                            success: false,
-                        });
-                    }
-                    else pool.query(
-                            "SELECT users.username, users.displayname, users.id FROM db.reposts JOIN users ON users.id=reposts.user WHERE post = ?",
-                            [id],
-                            (err, result) => {
-                                if (err) {
-                                    logger.error("Error in GET /posts/:id/reposts: ", err);
-                                    res.status(500).send({
-                                        message: "Error getting post reposts",
-                                        success: false,
-                                    })
-                                }
-                                else {
-                                    res.status(200).send({
-                                        success: true,
-                                        reposts: result,
-                                    })
-                                }
-                            }
-                        )
-                })
+                const queryResult = await query("SELECT id FROM db.posts WHERE id = ?", [id]);
+                if (queryResult.length === 0) {
+                    throw new Error("Post not found");
+                }
+                else {
+                    const queryResult2 = await query("SELECT users.username, users.displayname, users.id FROM db.reposts JOIN users ON users.id=reposts.user WHERE post = ?", [id]);
+                    res.status(200).send({
+                        success: true,
+                        reposts: queryResult2,
+                    })
+                }
             } catch (e) {
                 logger.error("Error in GET /posts/:id/reposts: ", e);
+                if (e.message === "Post not found") {
+                    res.status(404).send({
+                        message: "Post not found",
+                        success: false,
+                    });
+                }
                 res.status(500).send({
                     message: "Something went wrong",
-                    reason: e,
+                    reason: e.message,
                     success: false,
                 })
             }
@@ -66,83 +50,44 @@ module.exports = function routes(app, logger) {
             try {
                 const user = await jwt.verifyToken(req);
                 const { id } = req.params;
-                pool.query(
-                    "SELECT author FROM db.posts WHERE id = ?",
-                    [id],
-                    (err, result) => {
-                        if (err) {
-                            logger.error("Error in POST /posts/:id/repost: ", err);
-                            res.status(400).send({
-                                message: "Error getting post",
-                                success: false,
-                            })
-                        }
-                        else if (result.length === 0) {
-                            res.status(404).send({
-                                message: "Post not found",
-                                success: false,
-                            });
-                        }
-                        else {
-                            if (result[0].author === user.id) {
-                                res.status(401).send({
-                                    message: "You cannot repost your own post",
-                                    success: false,
-                                })
-                            } else {
-                                pool.query("SELECT id FROM db.reposts WHERE user = ? AND post = ?", [user.id, id], (err, result) => {
-                                        if (err) {
-                                            logger.error("Error in POST /posts/:id/repost: ", err);
-                                            res.status(400).send({
-                                                message: "Error getting post",
-                                                success: false,
-                                            })
-                                        }
-                                        else {
-                                            if (result.length > 0) {
-                                                res.status(400).send({
-                                                    message: "You have already reposted this post",
-                                                    success: false,
-                                                })
-                                            } else {
-                                                pool.query(
-                                                    "INSERT INTO db.reposts (post, user) VALUES (?, ?)",
-                                                    [id, user.id],
-                                                    (err, result) => {
-                                                        if (err) {
-                                                            logger.error("Error in POST /posts/:id/repost: ", err);
-                                                            res.status(400).send({
-                                                                message: "Error liking post",
-                                                                success: false,
-                                                            })
-                                                        }
-                                                        else {
-                                                            res.status(201).send({
-                                                                message: "Post reposted successfully",
-                                                                success: true,
-                                                            })
-                                                        }
-                                                    }
-                                                )
-                                            }
-                                        }
-                                    }
-                                )
-                            }
-                        }
+                const queryResult = await query("SELECT author FROM db.posts WHERE id = ?", [id]);
+                if (queryResult.length === 0) {
+                    throw new Error("Post not found");
+                }
+                else {
+                    const queryResult2 = await query("SELECT id FROM db.reposts WHERE user = ? AND post = ?", [user.id, id]);
+                    if (queryResult2.length > 0) {
+                        throw new Error("Already reposted");
                     }
-                )
+                    else {
+                        await query("INSERT INTO db.reposts (user, post) VALUES (?, ?)", [user.id, id]);
+                        res.status(201).send({
+                            success: true,
+                            message: "Reposted successfully",
+                        })
+                    }
+                }
             } catch (e) {
                 logger.error("Error in POST /posts/:id/repost: ", e);
-                if (e === "Invalid token") {
+                if (e.message === "Invalid token") {
                     res.status(401).send({
                         message: "Unauthorized",
                         success: false,
                     })
+                } else if (e.message === "Post not found") {
+                    res.status(404).send({
+                        message: "Post not found",
+                        success: false,
+                    });
+                } else if (e.message === "Already reposted") {
+                    res.status(403).send({
+                        message: "Already reposted",
+                        success: false,
+                    });
                 } else
                     res.status(500).send({
                         message: "Something went wrong",
-                        reason: e,
+                        reason: e.message,
                         success: false,
                     })
             }
@@ -158,57 +103,37 @@ module.exports = function routes(app, logger) {
             try {
                 const user = await jwt.verifyToken(req);
                 const { id } = req.params;
-                pool.query(
-                    "SELECT author FROM db.posts WHERE id = ?",
-                    [id],
-                    (err, result) => {
-                        if (err) {
-                            logger.error("Error in DELETE /posts/:id/repost: ", err);
-                            res.status(400).send({
-                                message: "Error getting post",
-                                success: false,
-                            })
-                        }
-                        else if (result.length === 0) {
-                            res.status(404).send({
-                                message: "Post not found",
-                                success: false,
-                            });
-                        }
-                        else {
-                            if (result[0].author === user.id) {
-                                res.status(401).send({
-                                    message: "You cannot unrepost your own post",
-                                    success: false,
-                                })
-                            } else {
-                                pool.query(
-                                    "DELETE FROM db.reposts WHERE post = ? AND user = ?",
-                                    [id, user.id],
-                                    (err, result) => {
-                                        if (err) {
-                                            logger.error("Error in DELETE /posts/:id/repost: ", err);
-                                            res.status(400).send({
-                                                message: "Error unreposting post",
-                                                success: false,
-                                            })
-                                        }
-                                        else {
-                                            res.status(204).send();
-                                        }
-                                    }
-                                )
-                            }
-                        }
+                const queryResult = await query("SELECT author FROM db.posts WHERE id = ?", [id]);
+                if (queryResult.length === 0) {
+                    throw new Error("Post not found");
+                }
+                else {
+                    const queryResult2 = await query("SELECT id FROM db.reposts WHERE user = ? AND post = ?", [user.id, id]);
+                    if (queryResult2.length === 0) {
+                        throw new Error("Not reposted");
                     }
-                )
+                    else {
+                        await query("DELETE FROM db.reposts WHERE user = ? AND post = ?", [user.id, id]);
+                        res.status(204).send();
+                    }
+                }
             } catch (e) {
                 logger.error("Error in DELETE /posts/:id/repost: ", e);
-                if (e === "Invalid token") {
+                if (e.message === "Invalid token") {
                     res.status(401).send({
                         message: "Unauthorized",
                         success: false,
                     })
+                } else if (e.message === "Post not found") {
+                    res.status(404).send({
+                        message: "Post not found",
+                        success: false,
+                    });
+                } else if (e.message === "Not reposted") {
+                    res.status(403).send({
+                        message: "Not reposted",
+                        success: false,
+                    });
                 } else
                     res.status(500).send({
                         message: "Something went wrong",
