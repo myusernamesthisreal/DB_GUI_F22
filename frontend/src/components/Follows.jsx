f089d44a66331afbb16de3738add295d1f40802d
snehagalex <71729811+snehagalex@users.noreply.github.com>
2022-10-26 19:53:58 -0500

    Update Follows.jsx
    added handle functions for following, followers, and unfollow to Follow.jsx

--- frontend/src/components/Follows.jsx
+++ frontend/src/components/Follows.jsx
@@ -6,8 +6,23 @@ function Follow() {
     const [user, setUser] = useState("");
     const api = new Api();
 
+    const handleFollowing = async () => {
+        const req = await api.following(user);
+        if (req.success) window.location.href="/";
+    }
+
+    const handleFollowers = async () => {
+        const req = await api.followers(user);
+        if (req.success) window.location.href="/";
+    }
+
     const handleFollow = async () => {
         const req = await api.follow(user);
+        if (req.success) window.location.href="/";
+    }
+
+    const handleUnfollow = async () => {
+        const req = await api.unfollow(user);
         if (req.success) window.location.href="/";
     }
 
@@ -15,10 +30,8 @@ function Follow() {
         <>
             <Box sx={{ width: '50%', border: 1, p: 2.5, justifyContent: "center", marginX: "auto", marginTop: "5rem"}}>
                 <Stack direction="column" alignItems="stretch" justifyContent="flex-start" spacing={1.5}>
-                <Typography sx={{ color: "grey"}}>
-                        Sign In
-                </Typography>
-                <TextField required id="username-box" label="Username" type="standard" value={username} onChange={(e) => setUsername(e.target.value)} />
+                <button required id="follow-button" label="Follow" type="standard" value={user} onChange={(e) => follow(e.target.value)} >Follow</button>
+                <button required id="get-followers-button" label="getFollowers" type="standard" value={user} onChange={(e) => follow(e.target.value)}>Get Followers</button>
             <TextField required id="password-box"  label="Password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
                     <Button variant="contained" color="primary" onClick={handleLogIn}>Sign In</Button>
 
