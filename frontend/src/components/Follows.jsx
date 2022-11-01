e8b0e29e4fca2548a5bcb58a28b8120301be1cd9
snehagalex <71729811+snehagalex@users.noreply.github.com>
2022-11-01 11:34:32 -0500

    Update Follows.jsx

--- frontend/src/components/Follows.jsx
+++ frontend/src/components/Follows.jsx
@@ -28,15 +28,11 @@ function Follow() {
 
     return (
         <>
-            <Box sx={{ width: '50%', border: 1, p: 2.5, justifyContent: "center", marginX: "auto", marginTop: "5rem"}}>
+            <Box sx={{ width: '50%', border: 1, p: 2.5, justifyContent: "left", marginX: "auto", marginTop: "5rem"}}>
                 <Stack direction="column" alignItems="stretch" justifyContent="flex-start" spacing={1.5}>
-                <button required id="follow-btn" label="Follow" type="standard" value={user} onClick={(e) => follow(e.target.value)} >Follow</button>
-                <button required id="get-followers-btn" label="getFollowers" type="standard" value={user} onClick={(e) => followers(e.target.value)}>Your Followers</button>
-                <button required id="get-following-btn" label="getFollowing" type="standard" value={user} onClick={(e) => following(e.target.value)}>Accounts You Follow</button>
-            <TextField required id="password-box"  label="Password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-                    <Button variant="contained" color="primary" onClick={handleLogIn}>Sign In</Button>
-
-                    <Button variant="text" size="small" onClick={handleSignUp}>Don't have an account? Sign up</Button>
+                    <button required id="follow-btn" label="Follow" type="standard" value={user} onClick={(e) => follow(e.target.value)} >Follow</button>
+                    <button required id="get-followers-btn" label="getFollowers" type="standard" value={user} onClick={(e) => followers(e.target.value)}>Your Followers</button>
+                    <button required id="get-following-btn" label="getFollowing" type="standard" value={user} onClick={(e) => following(e.target.value)}>Accounts You Follow</button>
                 </Stack>
             </Box>
         </>
