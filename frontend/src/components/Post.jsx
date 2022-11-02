d9651abfef9bdccbdca0edfea123a10cf6100ab3
NicoleJadeVivier <nvivier@mail.smu.edu>
2022-11-02 09:05:15 -0500

    added post format

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -1,13 +1,32 @@
 import React from 'react';
-import Box from '@mui/material/Box'
-import Avatar from '@mui/icons-material'
-import { grey } from '@mui/material/colors';
+import Avatar from '@mui/material/Avatar'
+import { blueGrey } from '@mui/material/colors';
+import Typography from '@mui/material/Typography';
+import { ListItem, ListItemAvatar, ListItemText } from '@mui/material';
 
 export const Post = (props) => {
     return <>
-    <Box>
-        
-    </Box>
+        <ListItem alignItems="flex-start">
+            <ListItemAvatar>
+                <Avatar sx={{ bgcolor: blueGrey }}> {props.post.authorname}</Avatar>
+            </ListItemAvatar>
+            <ListItemText
+                primary={props.post.authordisplayname}
+                secondary={
+                    <React.Fragment>
+                        <Typography
+                            sx={{ display: 'inline' }}
+                            component="span"
+                            variant="body2"
+                            color="text.primary"
+                        >
+                            {props.post.authorname}
+                        </Typography>
+                        {props.post.body}
+                    </React.Fragment>
+                }
+            />
+        </ListItem>
     </>
 
 }
