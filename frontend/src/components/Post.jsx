06de0f75a98e5d342195c6541d6b4b844d025730
NicoleJadeVivier <nvivier@mail.smu.edu>
2022-11-02 14:34:17 -0500

    centering content, need to make sure text does not overflow

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import Avatar from '@mui/material/Avatar'
 import { blueGrey } from '@mui/material/colors';
 import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
 import { ListItem, ListItemAvatar, ListItemText } from '@mui/material';
+import { boxSizing } from '@mui/system';
 
 export const Post = (props) => {
     const [time, timestamp] = props.post.timestamp.split('T');
@@ -10,36 +12,39 @@ export const Post = (props) => {
     const date = new Date(year, month - 1, day);
     const dString = date.toDateString();
 
-    
+
 
     return <>
-        <ListItem alignItems="flex-start">
-            <ListItemAvatar>
-                <Avatar sx={{ bgcolor: blueGrey }}> {props.post.authorname}</Avatar>
-            </ListItemAvatar>
-            <ListItemText
-                primary={
-                <React.Fragment>
-                    <Typography sx={{display: 'inline'}}>
-                        {props.post.authordisplayname}
-                    </Typography> @
-                    {props.post.authorname}
-                </React.Fragment>}
-                secondary={
-                    <React.Fragment>
-                        <Typography
-                            sx={{ display: 'inline' }}
-                            component="span"
-                            variant="body2"
-                            color="text.primary"
-                        >
-                            {dString}
-                        </Typography> --
-                        {props.post.body}
-                    </React.Fragment>
-                }
-            />
-        </ListItem>
+        <Box sx={{justifyContent: "center", border: 1, borderRadius: "10px", width: '75%', marginX: "auto", marginTop: "1rem", bgcolor:'background.paper'}}>
+            <ListItem alignItems="flex-start">
+                <ListItemAvatar>
+                    <Avatar sx={{ bgcolor: blueGrey }}> {props.post.authorname}</Avatar>
+                </ListItemAvatar>
+                <ListItemText
+                    primary={
+                        <React.Fragment>
+                            <Typography sx={{ display: 'inline' }}>
+                                {props.post.authordisplayname}
+                            </Typography> @
+                            <Typography sx={{ display: 'inline' }} variant="body2">
+                                {props.post.authorname}
+                            </Typography>
+                        </React.Fragment>}
+                    secondary={
+                        <React.Fragment>
+                            <Typography
+                                sx={{ display: 'inline' }}
+                                component="span"
+                                variant="body2"
+                            >
+                                {dString}
+                            </Typography> --
+                            {props.post.body}
+                        </React.Fragment>
+                    }
+                />
+            </ListItem>
+        </Box>
     </>
 
 }
