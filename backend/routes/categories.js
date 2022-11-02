fa005f59a91f790a80203f9bf663dcb6b6ab7a33
myusernamesthisreal <myusernamesthis@gmail.com>
2022-11-02 10:39:59 -0500

    PUT and GET specific routes

--- backend/routes/categories.js
+++ backend/routes/categories.js
@@ -63,8 +63,8 @@ module.exports = function routes(app, logger) {
                     const currentCategoriesArray = currentCategories.map((category) => category.categoryname);
                     const categoriesToAdd = validatedCategories.filter((category) => !currentCategoriesArray.includes(category));
                     const categoriesToRemove = currentCategoriesArray.filter((category) => validatedCategories.includes(category));
-                    logger.info("Categories to add: ", {message: categoriesToAdd});
-                    logger.info("Categories to remove: ", {message:categoriesToRemove});
+                    logger.info("Categories to add: ", { message: categoriesToAdd });
+                    logger.info("Categories to remove: ", { message: categoriesToRemove });
                     if (categoriesToAdd.length) await query("INSERT INTO db.categories (post, categoryname) VALUES ?",
                         [categoriesToAdd.map((category) => [id, category])]);
                     if (categoriesToRemove.length) await query("DELETE FROM db.categories WHERE post = ? AND categoryname IN (?)",
@@ -75,7 +75,7 @@ module.exports = function routes(app, logger) {
                         message: "Categories updated",
                         categories: newCategories.map((category) => category.categoryname),
                     });
-                    
+
                 }
             } catch (e) {
                 logger.error("Error in PATCH /posts/:id/categories: ", e);
@@ -99,6 +99,98 @@ module.exports = function routes(app, logger) {
                 } else if (e.message === "No categories provided" || e.message === "Categories must be an array" || e.message === "Categories must be strings") {
                     res.status(400).send({
                         message: e.message,
+                        success: false,
+                    });
+                } else {
+                    res.status(500).send({
+                        message: "Something went wrong",
+                        reason: e.message,
+                        success: false,
+                    })
+                }
+            }
+        });
+
+    //PUT /posts/:id/categories (modify categories on post)
+    app.put("/posts/:id/categories",
+        /**
+         * @param {import('express').Request} req
+         * @param {import('express').Response} res
+         */
+        async (req, res) => {
+            try {
+                const { id } = req.params;
+                const user = await jwt.verifyToken(req);
+                const { categories } = req.body;
+                if (!categories) {
+                    throw new Error("No categories provided");
+                }
+                if (!Array.isArray(categories)) {
+                    throw new Error("Categories must be an array");
+                }
+                const validatedCategories = categories.map((category) => {
+                    if (typeof category !== "string") {
+                        throw new Error("Categories must be strings");
+                    }
+                    return category.toLowerCase();
+                });
+                const queryResult = await query("SELECT author FROM db.posts WHERE id = ?", [id]);
+                if (queryResult.length === 0) {
+                    throw new Error("Post not found");
+                }
+                else if (queryResult[0].author !== user.id) {
+                    throw new Error("Forbidden");
+                }
+                else {
+                    await query("DELETE FROM db.categories WHERE post = ?", [id]);
+                    await query("INSERT INTO db.categories (post, categoryname) VALUES ?",
+                        [validatedCategories.map((category) => [id, category])]);
+                    const newCategories = await query("SELECT categoryname FROM db.categories WHERE post = ?", [id]);
+                    res.status(200).send({
+                        success: true,
+                        message: "Categories updated",
+                        categories: newCategories.map((category) => category.categoryname),
+                    });
+                }
+            } catch (e) {
+                logger.error("Error in PATCH /posts/:id/categories: ", e);
+                if (e.message === "Post not found") {
+                    res.status(404).send({
+                        message: "Post not found",
+                        success: false,
+                    });
+                }
+                else if (e.message === "Invalid token") {
+                    res.status(401).send({
+                        message: "Unauthorized",
+                        success: false,
+                    });
+                }
+            }
+        });
+
+    // GET /posts/:id/categories (get categories on post)
+    app.get("/posts/:id/categories",
+        /**
+         * @param {import('express').Request} req
+         * @param {import('express').Response} res
+         */
+        async (req, res) => {
+            try {
+                const { id } = req.params;
+                const queryResult = await query("SELECT categoryname FROM db.categories WHERE post = ?", [id]);
+                if (queryResult.length === 0) {
+                    throw new Error("Post not found");
+                }
+                res.status(200).send({
+                    success: true,
+                    categories: queryResult.map((category) => category.categoryname),
+                })
+            } catch (e) {
+                logger.error("Error in GET /posts/:id/categories: ", e);
+                if (e.message === "Post not found") {
+                    res.status(404).send({
+                        message: "Post not found",
                         success: false,
                     });
                 } else {
