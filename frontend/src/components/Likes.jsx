5216030d6a38b33d9a7246bfc3abb8bf6d9b43b0
snehagalex <71729811+snehagalex@users.noreply.github.com>
2022-11-02 20:11:32 -0500

    Update Likes.jsx
    fixed like button

--- frontend/src/components/Likes.jsx
+++ frontend/src/components/Likes.jsx
@@ -2,18 +2,18 @@ import React, { useState } from 'react'
 import { Button } from '@mui/material'
 import { Api } from '../api'
 
-export function Like(...isLiked) {
+export function Like(props) {
     const [liked, likePost] = useState(false);
     const api = new Api();
     
     const handleLikes = async () => {
-        const req = await api.like(id);
+        const req = await api.like(props.id);
         if (req.success) {
             likePost(!liked);
         }
     }
 
-    const handleGetLikes = async () => {
+    /*const handleGetLikes = async () => {
         const req = await api.like(id);
         if (req.success) window.location.href="/";
     }
@@ -27,6 +27,9 @@ export function Like(...isLiked) {
         const req = await api.getUserLikedPosts();
         if (req.success) window.location.href="/";
     }
+    <Button variant="outlined" size="small" onClick={() => props.user?.username ? null : window.location.href="/Signup"}>Like</Button>
+
+    */
 
     return <>
         <Button variant="outlined" 
