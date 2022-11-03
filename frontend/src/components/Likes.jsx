8f1868f4fb461c30f34313756a6c2d788507604b
snehagalex <71729811+snehagalex@users.noreply.github.com>
2022-11-02 19:24:07 -0500

    Update Likes.jsx
    added handle functions to Likes.jsx

--- frontend/src/components/Likes.jsx
+++ frontend/src/components/Likes.jsx
@@ -4,9 +4,25 @@ import { Api } from '../api'
 
 function Like() {
     const api = new Api();
+    
+    const handleLikes = async () => {
+        const req = await api.like(id);
+        if (req.success) window.location.href="/";
+    }
 
-    const handleLikes = () => {
-        
+    const handleGetLikes = async () => {
+        const req = await api.like(id);
+        if (req.success) window.location.href="/";
+    }
+
+    const handleGetLikedPosts = async () => {
+        const req = await api.getLikes(id);
+        if (req.success) window.location.href="/";
+    }
+
+    const handlegetUserLikedPosts = async () => {
+        const req = await api.getUserLikedPosts();
+        if (req.success) window.location.href="/";
     }
 
     return <>
