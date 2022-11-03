6ec27eb4caac76bbc06ae3a114ae0e0ef7e88abf
bskav <brandon@zelus.io>
2022-11-02 19:04:49 -0500

    Updated saves and reposts logic and routes

--- backend/routes/reposts.js
+++ backend/routes/reposts.js
@@ -1,9 +1,10 @@
 const pool = require('../db');
+const { promisify } = require("util");
 const jwt = require("../jwt");
-const promisify = require("util").promisify;
 
 module.exports = function routes(app, logger) {
     const query = promisify(pool.query).bind(pool);
+
     // GET /posts/:id/reposts (get reposts on a post)
     app.get("/posts/:id/reposts",
         /**
@@ -14,29 +15,27 @@ module.exports = function routes(app, logger) {
             try {
                 const { id } = req.params;
                 const queryResult = await query("SELECT id FROM db.posts WHERE id = ?", [id]);
-                if (queryResult.length === 0) {
+                if (queryResult.length === 0)
                     throw new Error("Post not found");
-                }
-                else {
-                    const queryResult2 = await query("SELECT users.username, users.displayname, users.id FROM db.reposts JOIN users ON users.id=reposts.user WHERE post = ?", [id]);
-                    res.status(200).send({
-                        success: true,
-                        reposts: queryResult2,
-                    })
-                }
+                const reposts = await query("SELECT users.id, users.username, users.displayname FROM reposts JOIN users ON reposts.user = users.id WHERE post = ?", [id]);
+                res.status(200).send({
+                    message: "Reposts fetched",
+                    success: true,
+                    reposts,
+                })
             } catch (e) {
                 logger.error("Error in GET /posts/:id/reposts: ", e);
                 if (e.message === "Post not found") {
                     res.status(404).send({
                         message: "Post not found",
                         success: false,
-                    });
-                }
-                res.status(500).send({
-                    message: "Something went wrong",
-                    reason: e.message,
-                    success: false,
-                })
+                    })
+                } else
+                    res.status(500).send({
+                        message: "Something went wrong",
+                        reason: e.message,
+                        success: false,
+                    })
             }
         })
 
@@ -51,22 +50,18 @@ module.exports = function routes(app, logger) {
                 const user = await jwt.verifyToken(req);
                 const { id } = req.params;
                 const queryResult = await query("SELECT author FROM db.posts WHERE id = ?", [id]);
-                if (queryResult.length === 0) {
+                if (queryResult.length === 0)
                     throw new Error("Post not found");
-                }
-                else {
-                    const queryResult2 = await query("SELECT id FROM db.reposts WHERE user = ? AND post = ?", [user.id, id]);
-                    if (queryResult2.length > 0) {
-                        throw new Error("Already reposted");
-                    }
-                    else {
-                        await query("INSERT INTO db.reposts (user, post) VALUES (?, ?)", [user.id, id]);
-                        res.status(201).send({
-                            success: true,
-                            message: "Reposted successfully",
-                        })
-                    }
-                }
+                if (queryResult[0].author === user.id)
+                    throw new Error("Cannot repost your own post");
+                const repostResult = await query("SELECT id FROM db.reposts WHERE post = ? AND user = ?", [id, user.id]);
+                if (repostResult.length > 0)
+                    throw new Error("Already reposted");
+                await query("INSERT INTO db.reposts (user, post) VALUES (?, ?)", [user.id, id]);
+                res.status(201).send({
+                    message: "Post reposted",
+                    success: true,
+                })
             } catch (e) {
                 logger.error("Error in POST /posts/:id/repost: ", e);
                 if (e.message === "Invalid token") {
@@ -74,20 +69,25 @@ module.exports = function routes(app, logger) {
                         message: "Unauthorized",
                         success: false,
                     })
+                } else if (e.message === "Cannot repost your own post") {
+                    res.status(403).send({
+                        message: "Cannot repost your own post",
+                        success: false,
+                    })
                 } else if (e.message === "Post not found") {
                     res.status(404).send({
                         message: "Post not found",
                         success: false,
-                    });
+                    })
                 } else if (e.message === "Already reposted") {
-                    res.status(403).send({
+                    res.status(409).send({
                         message: "Already reposted",
                         success: false,
-                    });
+                    })
                 } else
                     res.status(500).send({
                         message: "Something went wrong",
-                        reason: e.message,
+                        reason: e,
                         success: false,
                     })
             }
@@ -104,19 +104,10 @@ module.exports = function routes(app, logger) {
                 const user = await jwt.verifyToken(req);
                 const { id } = req.params;
                 const queryResult = await query("SELECT author FROM db.posts WHERE id = ?", [id]);
-                if (queryResult.length === 0) {
+                if (queryResult.length === 0)
                     throw new Error("Post not found");
-                }
-                else {
-                    const queryResult2 = await query("SELECT id FROM db.reposts WHERE user = ? AND post = ?", [user.id, id]);
-                    if (queryResult2.length === 0) {
-                        throw new Error("Not reposted");
-                    }
-                    else {
-                        await query("DELETE FROM db.reposts WHERE user = ? AND post = ?", [user.id, id]);
-                        res.status(204).send();
-                    }
-                }
+                await query("DELETE FROM db.reposts WHERE user = ? AND post = ?", [user.id, id]);
+                res.status(204).send();
             } catch (e) {
                 logger.error("Error in DELETE /posts/:id/repost: ", e);
                 if (e.message === "Invalid token") {
@@ -128,16 +119,138 @@ module.exports = function routes(app, logger) {
                     res.status(404).send({
                         message: "Post not found",
                         success: false,
-                    });
-                } else if (e.message === "Not reposted") {
-                    res.status(403).send({
-                        message: "Not reposted",
+                    })
+                }
+                else
+                    res.status(500).send({
+                        message: "Something went wrong",
+                        reason: e.message,
                         success: false,
+                    })
+            }
+        }
+    )
+
+    // PATCH /posts/:id/reposts (repost/unrepost post)
+    app.patch("/posts/:id/reposts",
+        /**
+         * @param {import('express').Request} req
+         * @param {import('express').Response} res
+         */
+        async (req, res) => {
+            try {
+                const user = await jwt.verifyToken(req);
+                const { id } = req.params;
+                const queryResult = await query("SELECT author FROM db.posts WHERE id = ?", [id]);
+                if (queryResult.length === 0)
+                    throw new Error("Post not found");
+                if (queryResult[0].author === user.id)
+                    throw new Error("Cannot repost your own post");
+                const repostResult = await query("SELECT id FROM db.reposts WHERE post = ? AND user = ?", [id, user.id]);
+                if (repostResult.length > 0)
+                {
+                    await query("DELETE FROM db.reposts WHERE user = ? AND post = ?", [user.id, id]);
+                    res.status(200).send({
+                        message: "Post unreposted",
+                        success: true,
                     });
+                }
+                else
+                {
+                    await query("INSERT INTO db.reposts (user, post) VALUES (?, ?)", [user.id, id]);
+                    res.status(201).send({
+                        message: "Post reposted",
+                        success: true,
+                    })
+                }
+            }
+            catch (e) {
+                logger.error("Error in PATCH /posts/:id/reposts: ", e);
+                if (e.message === "Invalid token") {
+                    res.status(401).send({
+                        message: "Unauthorized",
+                        success: false,
+                    })
+                } else if (e.message === "Cannot repost your own post") {
+                    res.status(403).send({
+                        message: "Cannot repost your own post",
+                        success: false,
+                    })
+                } else if (e.message === "Post not found") {
+                    res.status(404).send({
+                        message: "Post not found",
+                        success: false,
+                    })
                 } else
                     res.status(500).send({
                         message: "Something went wrong",
                         reason: e,
+                        success: false,
+                    })
+            }
+        });
+
+    // GET /users/:id/reposts (get reposted posts of a user)
+    app.get("/users/:id/reposts",
+        /**
+         * @param {import('express').Request} req
+         * @param {import('express').Response} res
+         */
+        async (req, res) => {
+            try {
+                const { id } = req.params;
+                const queryResult = await query("SELECT id FROM db.users WHERE id = ?", [id]);
+                if (queryResult.length === 0)
+                    throw new Error("User not found");
+                const reposts = await query("SELECT posts.*, users.username AS authorname, users.displayname AS authordisplayname FROM reposts JOIN posts ON reposts.post = posts.id JOIN users ON users.id = reposts.user WHERE users.id = ?", [id]);
+                res.status(200).send({
+                    message: "Reposts fetched",
+                    success: true,
+                    reposts,
+                })
+            } catch (e) {
+                logger.error("Error in GET /users/:id/reposts: ", e);
+                if (e.message === "User not found") {
+                    res.status(404).send({
+                        message: "User not found",
+                        success: false,
+                    })
+                } else
+                    res.status(500).send({
+                        message: "Something went wrong",
+                        reason: e.message,
+                        success: false,
+                    })
+            }
+        }
+    )
+
+    // GET /users/reposts (get reposted posts of current user)
+    app.get("/users/reposts",
+        /**
+         * @param {import('express').Request} req
+         * @param {import('express').Response} res
+         */
+        async (req, res) => {
+            try {
+                const user = await jwt.verifyToken(req);
+                const reposts = await query("SELECT posts.*, users.username AS authorname, users.displayname AS authordisplayname FROM reposts JOIN posts ON reposts.post = posts.id JOIN users ON users.id = reposts.user WHERE users.id = ?", [user.id]);
+                res.status(200).send({
+                    message: "Reposts fetched",
+                    success: true,
+                    reposts,
+                })
+            } catch (e) {
+                logger.error("Error in GET /users/reposts: ", e);
+                if (e.message === "Invalid token") {
+                    res.status(401).send({
+                        message: "Unauthorized",
+                        success: false,
+                    })
+                } else
+                    res.status(500).send({
+                        message: "Something went wrong",
+                        reason: e.message,
                         success: false,
                     })
             }
