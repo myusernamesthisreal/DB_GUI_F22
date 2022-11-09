aa584d49866dd31bf85fd7eec7dacdb2c3b89810
NicoleJadeVivier <nvivier@mail.smu.edu>
2022-11-09 14:18:14 -0600

    replacing cancel button with a cancel icon and adding styling

--- frontend/src/components/CreatePost.jsx
+++ frontend/src/components/CreatePost.jsx
@@ -3,10 +3,12 @@ import { Api } from '../api';
 import Box from "@mui/material/Box";
 import Avatar from "@mui/material/Avatar";
 import Typography from "@mui/material/Typography";
-import { ListItem, ListItemAvatar, ListItemText } from '@mui/material';
+import { IconButton, ListItem, ListItemAvatar, ListItemText, Button } from '@mui/material';
 import TextField from "@mui/material/TextField";
 import Stack from "@mui/material/Stack";
 import ListItemButton from '@mui/material/Button';
+import CancelIcon from '@mui/icons-material/Cancel';
+import Cancel from '@mui/icons-material/Cancel';
 
 
 export function CreatePost(props) {
@@ -19,16 +21,25 @@ export function CreatePost(props) {
         const req = await api.makePost(text, categories);
         if (req.success) window.location.href = "/";
     }
-   
+
     const handleCancelClick = async () => {
         if (window.confirm("Do you want to cancel this post?")) {
             window.location.href = "/";
-        } 
+        }
     }
 
     return (
         <>
             <Box sx={{ justifyContent: "center", border: 1, borderRadius: "10px", width: '75%', marginX: "auto", marginTop: "3.5rem", bgcolor: 'background.paper', p: "1rem" }}>
+                <Box sx={{ display: "flex" }} >
+                    <IconButton sx={{ justifyContent: "start" }}>
+                        <CancelIcon sx={{ display: "block", marginLeft: "8px" }} onClick={handleCancelClick}></CancelIcon>
+                    </IconButton>
+                    <Box sx={{ flexGrow: 1 }} />
+                    <Button sx={{ justifyContent: "start" }} variant="contained" size="small" onClick={handleNewPost}>
+                        <ListItemText primary="Post" />
+                    </Button>
+                </Box>
                 <ListItem alignItems="flex-start">
                     <ListItemAvatar>
                         <Avatar src="https://i.imgur.com/KNE5lGg.jpg" />
@@ -36,18 +47,15 @@ export function CreatePost(props) {
                     <ListItemText>
                         <Stack>
                             <Typography>{`@${props.user?.username}`}</Typography>
-                            <TextField id="standard-basic" multiline rows={4} label="What's happening?" variant="standard" onChange={(e) => setText(e.target.value)}/>
+                            <TextField id="standard-basic" multiline rows={4} label="What's happening?" variant="standard" onChange={(e) => setText(e.target.value)} />
                             <TextField id="standar-basic" label="Enter Categories (comma delimeted, no space)" variant="standard" onChange={(e) => setCategories(e.target.value.split(','))} />
                         </Stack>
                     </ListItemText>
-                    <ListItemButton variant="contained" size="small" onClick={ handleNewPost }>
-                        <ListItemText primary="Post" />
-                    </ListItemButton>
+
+
                 </ListItem>
-                <ListItemButton variant="contained" size="small" onClick={ handleCancelClick }>
-                    <ListItemText primary="Cancel" />
-                </ListItemButton>
             </Box>
+
         </>
     );
 }
