8770bd02dad70956fd494bbb9c282989f2d45f1c
NicoleJadeVivier <nvivier@mail.smu.edu>
2022-11-09 14:34:30 -0600

    Merge resolve

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Avatar from '@mui/material/Avatar'
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
@@ -7,14 +7,20 @@ import Button from '@mui/material/Button'
 import { Like } from './Likes';
 
 export const Post = (props) => {
-    const [time, setTime] = useState(props?.post.timestamp);
+    const [time, setTime] = useState("");
+    useEffect(() => {
+        const t = Date.parse(props?.post.timestamp);
+
+        const tzOffset = new Date().getTimezoneOffset() * 60000;
+        setTime(new Date(t - tzOffset).toLocaleString("en-us"));
+    }, []);
 
 
     return <>
-        <Box sx={{justifyContent: "center", border: 1, borderRadius: "10px", width: '75%', marginX: "auto", marginTop: "1rem", bgcolor:'background.paper'}}>
+        <Box sx={{ justifyContent: "center", border: 1, borderRadius: "10px", width: '75%', marginX: "auto", marginTop: "1rem", bgcolor: 'background.paper' }}>
             <ListItem alignItems="flex-start">
                 <ListItemAvatar>
-                    <Avatar src="https://i.imgur.com/KNE5lGg.jpg"/>
+                    <Avatar src="https://i.imgur.com/KNE5lGg.jpg" />
                 </ListItemAvatar>
                 <ListItemText
                     primary={
@@ -29,13 +35,13 @@ export const Post = (props) => {
                     secondary={
                         <React.Fragment>
                             <Typography
-                                sx={{ display: 'inline', overflow:"hidden" }}
+                                sx={{ display: 'inline', overflow: "hidden" }}
                                 component="span"
                                 variant="body2"
                             >
-                               {time ? new Date(time).toLocaleString("en-us") : null}
+                                {time ? new Date(time).toLocaleString("en-us") : null}
                             </Typography> --
-                            <Box sx={{overflow:"hidden"}}> {props.post.body} </Box>
+                            <Box sx={{ overflow: "hidden" }}> {props.post.body} </Box>
                         </React.Fragment>
                     }
                 />
