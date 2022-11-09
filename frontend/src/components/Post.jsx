e72a2578b39e745d485dde6eeaddb66651285802
NicoleJadeVivier <nvivier@mail.smu.edu>
2022-11-09 14:50:15 -0600

    feat: Links to user page from post

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -6,6 +6,7 @@ import { getNativeSelectUtilityClasses, ListItem, ListItemAvatar, ListItemText }
 import Button from '@mui/material/Button';
 import { Like } from './Likes';
 import { Repost } from './Repost';
+import { Link } from "react-router-dom"
 
 export const Post = (props) => {
     const [time, setTime] = useState("");
@@ -23,32 +24,34 @@ export const Post = (props) => {
                 <ListItemAvatar>
                     <Avatar src="https://i.imgur.com/KNE5lGg.jpg" />
                 </ListItemAvatar>
-                <ListItemText
-                    primary={
-                        <React.Fragment>
-                            <Typography sx={{ display: 'inline' }}>
-                                {props.post.authordisplayname}
-                            </Typography> @
-                            <Typography sx={{ display: 'inline' }} variant="body2">
-                                {props.post.authorname}
-                            </Typography>
-                        </React.Fragment>}
-                    secondary={
-                        <React.Fragment>
-                            <Typography
-                                sx={{ display: 'inline', overflow: "hidden" }}
-                                component="span"
-                                variant="body2"
-                            >
-                                {time ? new Date(time).toLocaleString("en-us") : null}
-                            </Typography> --
-                            <Box sx={{ overflow: "hidden" }}> {props.post.body} </Box>
-                        </React.Fragment>
-                    }
-                />
+                <Link style={{textDecoration:"none", color: "inherit"}} to={`/user/${props.post.author}`}>
+                    <ListItemText
+                        primary={
+                            <React.Fragment>
+                                <Typography sx={{ display: 'inline' }}>
+                                    {props.post.authordisplayname}
+                                </Typography> @
+                                <Typography sx={{ display: 'inline' }} variant="body2">
+                                    {props.post.authorname}
+                                </Typography>
+                            </React.Fragment>}
+                        secondary={
+                            <React.Fragment>
+                                <Typography
+                                    sx={{ display: 'inline', overflow: "hidden" }}
+                                    component="span"
+                                    variant="body2"
+                                >
+                                    {time ? new Date(time).toLocaleString("en-us") : null}
+                                </Typography> --
+                                <Box sx={{ overflow: "hidden" }}> {props.post.body} </Box>
+                            </React.Fragment>
+                        }
+                    />
+                </Link>
             </ListItem>
-            {props.user.id === props.post.author ? null : <Like post={props.post} /> }
-            {props.user.id === props.post.author ? null : <Repost post={props.post} /> }
+            {props.user.id === props.post.author ? null : <Like post={props.post} />}
+            {props.user.id === props.post.author ? null : <Repost post={props.post} />}
             <Button variant="outlined" size="small">Bookmark</Button>
             <Button variant="outlined" size="small">Comment</Button>
         </Box>
