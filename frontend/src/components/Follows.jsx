ee0b1079ad9999de1272dd47a6a34df083ad432a
snehagalex <71729811+snehagalex@users.noreply.github.com>
2022-11-09 14:16:00 -0600

    Update Follows.jsx
    added handle functions

--- frontend/src/components/Follows.jsx
+++ frontend/src/components/Follows.jsx
@@ -6,17 +6,25 @@ export function Follow() {
     const [follow, setFollow] = useState(false);
     const api = new Api();
 
-    /*const handleFollowing = async () => {
-        const req = await api.follow(props.id);
-        if (req.success) {
-            followUser(!follow);
-        }
+    const handleGetFollowingCurrent = async () => {
+        const req = await api.getFollowingCurrent();
+        if (req.success) window.location.href="/";
     }
 
-    const handleFollowers = async () => {
-        const req = await api.followers(user);
+    const handleGetFollowingGivenID = async () => {
+        const req = await api.getFollowingGivenID(props.id);
         if (req.success) window.location.href="/";
-    }*/
+    }
+
+    const handleGetFollowersCurrent = async () => {
+        const req = await api.getFollowersCurrent();
+        if (req.success) window.location.href="/";
+    }
+
+    const handleGetFollowersGivenID = async () => {
+        const req = await api.getFollowersGivenID(props.id);
+        if (req.success) window.location.href="/";
+    }
 
     const handleFollow = async () => {
         const req = await api.follow(props.id);
@@ -25,18 +33,39 @@ export function Follow() {
         }
     }
 
-    /*const handleUnfollow = async () => {
-        const req = await api.unfollow(user);
-        if (req.success) window.location.href="/";
-    }*/
+    const handleUnfollow = async () => {
+        const req = await api.unfollow(props.id);
+        if (req.success) {
+            setFollow(!follow);
+        }
+    }
+
+    const handlePatchFollow = async () => {
+        const req = await api.patchFollow(props.id);
+        if (req.success) {
+            setFollow(!follow);
+        }
+    }
 
     return (
         <>
             <Button variant="outlined" 
                 style={{ backgroundColor: liked ? 'blue' : '',
                      color: liked ? 'white' : '', }}
-                onClick={handleFollowing}>
+                onClick={handleFollow}>
                 Follow
+            </Button>
+            <Button variant="outlined" 
+                style={{ backgroundColor: liked ? 'blue' : '',
+                     color: liked ? 'white' : '', }}
+                onClick={handleGetFollowingCurrent}>
+                Following
+            </Button>
+            <Button variant="outlined" 
+                style={{ backgroundColor: liked ? 'blue' : '',
+                     color: liked ? 'white' : '', }}
+                onClick={handleGetFollowersCurrent}>
+                Followers
             </Button>
         </>
     )
