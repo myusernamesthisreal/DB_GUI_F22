5070699f6e421a9c2f0bc38924d903101e1c4484
NicoleJadeVivier <nvivier@mail.smu.edu>
2022-11-09 18:34:51 -0600

    Revert "Adding a checkbox with all of the categories"
    This reverts commit b59e54e76a0d16eec9856edec5f26f0b5ad02f1b.

--- frontend/src/components/Homepage.jsx
+++ frontend/src/components/Homepage.jsx
@@ -3,9 +3,6 @@ import { Api } from "../api";
 import { Post } from "./";
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box'
-import FormGroup from '@mui/material/FormGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
 import Typography from '@mui/material/Typography'
 
 export const Homepage = (props) => {
@@ -13,42 +10,30 @@ export const Homepage = (props) => {
 
     const api = new Api();
     const [posts, setPosts] = useState([]);
-    const [categories, setCategories] = useState([]);
 
     const handleLoad = async () => {
         const data = await api.getPosts();
         console.log(data);
         if (data.success) setPosts(data.posts);
-
-        const dataCategories = await api.getAllCatgories();
-        if (dataCategories.success) setCategories(dataCategories.categories);
     }
 
     useEffect(() => {
         handleLoad();
     }, []);
 
-    
-
-
     const handlePostClick = async () => {
         props.user?.username ? window.location.href = "/make-post" : window.location.href = "/Signup";
     }
 
     return (
-        <Box sx={{display:"flex"}}>
-            <FormGroup>
-                {categories.map((category, index) => <FormControlLabel control={<Checkbox/>} label={`${category.name}`}/>)}
-            </FormGroup>
+        <>
             <Box sx={{ flexGrow: 1, textAlign: "right", margin: "1rem" }}>
                 <Button variant="contained" onClick={handlePostClick}>Post</Button>
             </Box>
-            <Box sx={{display: "block", maxWidth:"60%"}}>
             {
                 posts.map((post, index) => <Post key={index} post={post} user={props.user} />)
             }
-            </Box>
-        </Box>
+        </>
     )
 }
 
