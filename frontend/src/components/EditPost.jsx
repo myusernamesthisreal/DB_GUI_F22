0cd82e1c1271aad91bb8312c9f1156d071c66e58
snehagalex <71729811+snehagalex@users.noreply.github.com>
2022-11-16 19:37:39 -0600

    Update EditPost.jsx

--- frontend/src/components/EditPost.jsx
+++ frontend/src/components/EditPost.jsx
@@ -1,69 +1,92 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import Avatar from '@mui/material/Avatar'
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import { getNativeSelectUtilityClasses, ListItem, ListItemAvatar, ListItemText, Chip } from '@mui/material';
+import Button from '@mui/material/Button';
+import { Like } from './Likes';
+import { Repost } from './Repost';
+import { Link } from "react-router-dom"
+import { CommentsModal } from './CommentsModal';
 import { Api } from '../api';
-import Box from "@mui/material/Box";
-import Avatar from "@mui/material/Avatar";
-import Typography from "@mui/material/Typography";
-import { IconButton, ListItem, ListItemAvatar, ListItemText, Button } from '@mui/material';
-import TextField from "@mui/material/TextField";
-import Stack from "@mui/material/Stack";
-import ListItemButton from '@mui/material/Button';
-import CancelIcon from '@mui/icons-material/Cancel';
-import Cancel from '@mui/icons-material/Cancel';
-
-
-export function EditPost(props) {
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
+    
+export const EditPost = (props) => {
     const api = new Api();
     const [text, setText] = useState("");
-    const [categories, setCategories] = useState("");
-
-
-    const handleEditPost = async () => {
-        const req = await api.Post(text, categories);
-        if (req.success) window.location.href = "/";
-    }
-
-    const handleDeletePost = async () => {
-        const req = await api.deletePost(props.post.id);
-        if (req.success) window.location.href = "/";
-    }
-
-    const handleCancelEdit = async () => {
-        if (window.confirm("Do you want to cancel this edit?")) {
-            window.location.href = "/";
+    const [categories, setCategories] = useState("");const [time, setTime] = useState("");
+    const [open, setOpen] = useState(false);
+    
+        useEffect(() => {
+            const t = Date.parse(props?.post.timestamp);
+    
+            const tzOffset = new Date().getTimezoneOffset() * 60000;
+            setTime(new Date(t - tzOffset).toLocaleString("en-us"));
+        }, []);
+    
+        const handleEditPost = async () => {
+            const req = await api.Post(text, categories);
+            if (req.success) window.location.href = "/";
         }
-    }
-
-    return (
-        <>
-            <Box sx={{ justifyContent: "center", border: 1, borderRadius: "10px", width: '75%', marginX: "auto", marginTop: "3.5rem", bgcolor: 'background.paper', p: "1rem" }}>
-                <Box sx={{ display: "flex" }} >
-                    <IconButton sx={{ justifyContent: "start" }}>
-                        <CancelIcon sx={{ display: "block", marginLeft: "8px" }} onClick={handleCancelEdit}></CancelIcon>
-                    </IconButton>
-                    <Box sx={{ flexGrow: 1 }} />
-                    <Button sx={{ justifyContent: "start" }} variant="contained" size="small" onClick={handleEditPost}>
-                        <ListItemText primary="Post Edit" />
-                    </Button>
-                    <Button sx={{ justifyContent: "start" }} variant="contained" size="small" onClick={handleDeletePost}>
-                        <ListItemText primary="Delete Post" />
-                    </Button>
-                </Box>
-                <ListItem alignItems="flex-start">
-                    <ListItemAvatar>
-                        <Avatar src="https://i.imgur.com/KNE5lGg.jpg" />
-                    </ListItemAvatar>
-                    <ListItemText>
-                        <Stack>
-                            <Typography>{`@${props.user?.username}`}</Typography>
-                            <TextField id="standard-basic" multiline rows={4} label="What's happening?" variant="standard" defaultValue={`${props.user?.text}`} onChange={(e) => setText(e.target.value)} />
-                            <TextField id="standar-basic" label="Enter Categories (comma delimeted, no space)" variant="standard" defaultValue={`${props.user?.categories}`} onChange={(e) => setCategories(e.target.value.split(','))} />
-                        </Stack>
-                    </ListItemText>
-
-
-                </ListItem>
+    
+        const handleDeletePost = async () => {
+            const req = await api.deletePost(props.post.id);
+            if (req.success) window.location.href = "/";
+        }
+    
+        const handleCancelEdit = async () => {
+            if (window.confirm("Do you want to cancel this edit?")) {
+                window.location.href = "/";
+            }
+        }
+        return <>
+            <Box sx={{ justifyContent: "center", border: 1, borderRadius: "10px", overflow: "hidden", textOverflow: "ellipsis", whiteSpace: "nowrap", width: '80%', marginX: "auto", marginTop: "1rem", bgcolor: 'background.paper' }}>
+                <Link style={{ textDecoration: "none", color: "inherit", cursor: "pointer" }} to={`/posts/${props.post.id}`}>
+                    <ListItem alignItems="flex-start">
+                        <ListItemAvatar>
+                            <Avatar src="https://i.imgur.com/KNE5lGg.jpg" />
+                        </ListItemAvatar>
+    
+                        <ListItemText
+                            primary={
+                                <React.Fragment>
+                                    <Link style={{ textDecoration: "none", color: "inherit" }} to={`/users/${props.post.author}`}>
+                                        <Typography sx={{ display: 'inline' }}>
+                                            {props.post.authordisplayname}
+                                        </Typography> @
+                                        <Typography sx={{ display: 'inline' }} variant="body2">
+                                            {props.post.authorname}
+                                        </Typography>
+                                    </Link>
+                                </React.Fragment>}
+                            secondary={
+                                <React.Fragment>
+                                    <Typography
+                                        sx={{ display: 'inline', overflow: "hidden" }}
+                                        component="span"
+                                        variant="body2"
+                                    >
+                                        {time ? new Date(time).toLocaleString("en-us") : null}
+                                    </Typography> --
+                                    <Box sx={{ overflow: "hidden" }}> {props.post.body} </Box>
+                                    {props.post.categories.map((category, index) => <Chip sx={{ marginRight: "0.5rem", marginTop: "0.5rem" }} label={`${category}`} />)}
+                                </React.Fragment>
+                            }
+                        />
+                    </ListItem>
+                </Link>
+                {props.user?.id === props.post.author ? null : <Like post={props.post} />}
+                {props.user?.id === props.post.author ? null : <Repost post={props.post} />}
+                <Button variant="outlined" size="small">Bookmark</Button>
+                <Button variant="outlined" size="small" onClick={() => setOpen(true)}>Comment</Button>
+                {props.user?.id !== props.post.author ? null : 
+                    <Popup trigger={<Button variant="outlined" size="small">...</Button>} position="right center">
+                        <Button>Edit or Delete</Button>
+                    </Popup> 
+                }
+                <CommentsModal open={open} setOpen={setOpen} post={props.post}/>
             </Box>
-
+    
         </>
-    );
 }
