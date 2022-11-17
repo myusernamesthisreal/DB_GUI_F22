c264376e4d69b4af9731ca28c0c27eb85933a867
NicoleJadeVivier <nvivier@mail.smu.edu>
2022-11-16 19:48:45 -0600

    switching comment button with an icon

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -8,6 +8,7 @@ import { Like } from './Likes';
 import { Repost } from './Repost';
 import { Link } from "react-router-dom"
 import { CommentsModal } from './CommentsModal';
+import InsertCommentIcon from '@mui/icons-material/InsertComment';
 
 export const Post = (props) => {
     const [time, setTime] = useState("");
@@ -60,7 +61,9 @@ export const Post = (props) => {
             {props.user?.id === props.post.author ? null : <Like post={props.post} />}
             {props.user?.id === props.post.author ? null : <Repost post={props.post} />}
             <Button variant="outlined" size="small">Bookmark</Button>
-            <Button variant="outlined" size="small" onClick={() => setOpen(true)}>Comment</Button>
+            <Button variant="outlined" size="small" onClick={() => setOpen(true)}>
+                <InsertCommentIcon sx={{border:"none"}}></InsertCommentIcon>
+            </Button>
             <CommentsModal open={open} setOpen={setOpen} post={props.post}/>
         </Box>
 
