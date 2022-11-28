8f88173b07c2b9a8e73047053765dbd61f4a8a3c
snehagalex <71729811+snehagalex@users.noreply.github.com>
2022-11-28 14:47:38 -0600

    Update EditPost.jsx

--- frontend/src/components/EditPost.jsx
+++ frontend/src/components/EditPost.jsx
@@ -2,16 +2,23 @@ import React, { useState, useEffect } from 'react';
 import Avatar from '@mui/material/Avatar'
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import { getNativeSelectUtilityClasses, ListItem, ListItemAvatar, ListItemText, Chip } from '@mui/material';
 import Button from '@mui/material/Button';
 import { Link } from "react-router-dom";
 import { Api } from '../api';
+import { IconButton, ListItem, ListItemAvatar, ListItemText, Snackbar, Alert, Chip } from '@mui/material';
+import TextField from "@mui/material/TextField";
+import Stack from "@mui/material/Stack";
+import ListItemButton from '@mui/material/Button';
+import CancelIcon from '@mui/icons-material/Cancel';
+import Cancel from '@mui/icons-material/Cancel';
     
 export const EditPost = () => {
     const api = new Api();
     const [post, setPost] = useState(null);
-    const [text, setText] = useState("");
-    const [categories, setCategories] = useState("");const [time, setTime] = useState("");
+    const [text, setText] = useState(post?.text);
+    const [categories, setCategories] = useState(post?.categories);
+    const [currentCategory, setCurrentCategory] = useState("");
+    const [time, setTime] = useState("");
     const [error, setError] = useState(false);
     const [open, setOpen] = useState(false);
     
@@ -21,6 +28,14 @@ export const EditPost = () => {
             const tzOffset = new Date().getTimezoneOffset() * 60000;
             setTime(new Date(t - tzOffset).toLocaleString("en-us"));
         }, []);
+
+        const handleClose = (event, reason) => {
+            if (reason === 'clickaway') {
+                return;
+            }
+    
+            setOpen(false);
+        };
     
         const handleEditPost = async () => {
             const req = await api.Post(text, categories);
@@ -36,6 +51,12 @@ export const EditPost = () => {
                 setPost(postById);
             }
             else setError(true);
+        }
+
+        const handleDelete = (index) => {
+            const newCats = [...categories];
+            newCats.splice(index, 1);
+            setCategories(newCats);
         }
     
         useEffect(() => {
@@ -86,6 +107,46 @@ export const EditPost = () => {
                         <Button variant="outlined" size="small" onClick={handleCancelEdit}>Cancel Edit</Button>
                     </ListItem>
                 </Link>
+            </Box>
+
+
+
+            <Snackbar
+                open={open}
+                autoHideDuration={6000}
+                onClose={handleClose}
+                anchorOrigin={{ vertical: "top", horizontal: "left" }}>
+                <Alert onClose={handleClose} severity="error" sx={{ width: '100%' }}>{error}</Alert>
+            </Snackbar>
+            <Box sx={{ justifyContent: "center", border: 1, borderRadius: "10px", width: '75%', marginX: "auto", marginTop: "3.5rem", bgcolor: 'background.paper', p: "1rem" }}>
+                <Box sx={{ display: "flex" }} >
+                    <IconButton sx={{ justifyContent: "start" }}>
+                        <CancelIcon sx={{ display: "block", marginLeft: "8px" }} onClick={handleCancelEdit}></CancelIcon>
+                    </IconButton>
+                    <Box sx={{ flexGrow: 1 }} />
+                    <Button sx={{ justifyContent: "start" }} variant="contained" size="small" onClick={handleEditPost}>
+                        <ListItemText primary="Post" />
+                    </Button>
+                </Box>
+                <ListItem alignItems="flex-start">
+                    <ListItemAvatar>
+                        <Avatar src="https://i.imgur.com/KNE5lGg.jpg" />
+                    </ListItemAvatar>
+                    <ListItemText>
+                        <Stack>
+                            <Typography>{`@${post?.username}`}</Typography>
+                            <TextField id="standard-basic" multiline rows={4} label="What's happening?" variant="standard" onChange={(e) => setText(e.target.value)} />
+                            <Box sx={{display: "flex"}}>
+                                {categories.map((value, index) => {
+                                    return <Chip key={index} sx={{ marginRight: "0.5rem", marginTop: "0.5rem" }} label={`${value}`} onDelete={() => handleDelete(index)} />
+                                })}
+                            </Box>
+                            <TextField id="standar-basic" label="Enter Categories" variant="standard" value={currentCategory} onChange={(e) => handleEvent(e)} />
+                        </Stack>
+                    </ListItemText>
+
+
+                </ListItem>
             </Box>
     
         </>
