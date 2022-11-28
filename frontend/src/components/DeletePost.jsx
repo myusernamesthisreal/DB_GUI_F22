7fdb6eccdbbe6353b9d5771270ea7f705dffc1b9
snehagalex <71729811+snehagalex@users.noreply.github.com>
2022-11-28 15:51:50 -0600

    Update DeletePost.jsx
    Based DeletePost off of CommentsModal

--- frontend/src/components/DeletePost.jsx
+++ frontend/src/components/DeletePost.jsx
@@ -1,32 +1,85 @@
-import React, { useState, useEffect } from 'react';
-import Avatar from '@mui/material/Avatar'
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import { getNativeSelectUtilityClasses, ListItem, ListItemAvatar, ListItemText, Chip } from '@mui/material';
-import Button from '@mui/material/Button';
-import Modal from '@mui/material/Modal';
-import { Link } from "react-router-dom";
+import React, { useState } from 'react';
+import { Modal, Typography, Box, TextField, Button, IconButton, Snackbar, Alert } from '@mui/material';
+import { InsertComment, Cancel } from '@mui/icons-material';
 import { Api } from '../api';
-    
-export const DeletePost = (props) => {
+import { useWindowWidth } from "@react-hook/window-size";
+
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 350,
+    bgcolor: 'background.paper',
+    boxShadow: 24,
+    p: 4,
+};
+
+export const DeletePost = ({ open, setOpen, post }) => {
+    const [text, setText] = useState("");
+    const [errorMsg, setErrorMsg] = useState("");
+    const [alertOpen, setAlertOpen] = useState(false);
+    const screenWidth = useWindowWidth();
+
+
     const api = new Api();
-    const [open, setOpen] = useState(false);
-    
-        const handleDeletePost = async () => {
-            const req = await api.deletePost(props.post.id);
-            if (req.success) window.location.href = "/";
+
+    //need to send in the post as well
+    const handleNewComment = async () => {
+        const req = await api.deletePost(text, post.id);
+        if (req.status === 201) {
+            setOpen(false);
+            window.location.reload(false);
+        }
+        else {
+            const body = await req.json();
+            setErrorMsg(body.message);
+            setAlertOpen(true);
+        }
+    }
+
+    const handleClose = () => setOpen(false);
+
+    const handleAlertClose = (event, reason) => {
+        if (reason === 'clickaway') {
+            return;
         }
 
-        return <>
-            <Box sx={{ justifyContent: "center", border: 1, borderRadius: "10px", overflow: "hidden", textOverflow: "ellipsis", whiteSpace: "nowrap", width: '80%', marginX: "auto", marginTop: "1rem", bgcolor: 'background.paper' }}>
-                <Link style={{ textDecoration: "none", color: "inherit", cursor: "pointer" }} to={`/posts/${props.post.id}/edit`}>
-                    <Modal aria-labelledby="modal-title" aria-describedby="modal-description">
-                        <h2>Are you sure you want to delete this post?</h2>
-                        <Button id="modal-description" onClick={handleDeletePost}>Yes</Button>
-                        <Button id="modal-description">Cancel</Button>
-                    </Modal>
-                </Link>
+        setAlertOpen(false);
+    };
+
+    return <>
+        <Snackbar
+            open={alertOpen}
+            autoHideDuration={6000}
+            onClose={handleAlertClose}
+            anchorOrigin={{ vertical: "top", horizontal: "left" }}>
+            <Alert onClose={handleAlertClose} severity="error" sx={{ width: '100%' }}>{errorMsg}</Alert>
+        </Snackbar>
+        <Button size="small" onClick={() => setOpen(true)}>
+            <InsertComment sx={{ border: "none", outline: "none" }}></InsertComment>
+        </Button>
+        <Modal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+        >
+            <Box sx={style}>
+                <Box sx={{ display: "flex" }}>
+                    <IconButton sx={{ justifyContent: "start" }}>
+                        <Cancel sx={{ display: "block" }} onClick={handleClose}></Cancel>
+                    </IconButton>
+                </Box>
+                <Typography id="modal-modal-title" variant="h6" component="h2" sx={{ mb: "2rem" }}>
+                    Add a comment
+                </Typography>
+                <Typography>
+                    <TextField label="comment" type="standard" fullWidth="true" onChange={(e) => setText(e.target.value)} />
+                </Typography>
+                <Button variant="outlined" size="small" sx={{ mt: "2rem", mr: "1rem" }} onClick={handleNewComment}>Submit</Button>
             </Box>
-    
-        </>
+        </Modal>
+    </>
 }
